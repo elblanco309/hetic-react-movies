1ab82f7fa5528e8254a5a0dd54997e620d39a12f
Yaaqoub <yaaqoub@octicode.com>
2026-04-01 11:41:27 +0200

    use effect

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from 'react'
+
 function About() {
+    const [todo, setTodo] = useState({});
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => setTodo(json))
+    });
+
     return (
         <div>
             <h1>About Us</h1>
             <p>Hetic school</p>
+
+            <h2>Todo</h2>
+            <ol>
+                <li>User ID: {todo.userId}</li>
+                <li>ID: {todo.id}</li>
+                <li>Title: {todo.title}</li>
+                <li>Completed: {todo.completed?.toString()}</li>
+            </ol>
         </div>
     );
 }

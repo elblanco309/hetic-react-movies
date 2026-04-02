28ee3959ac86ec8401a8b43e267b5d27a586b578
Emile <emilebj39@gmail.com>
2026-04-02 10:55:12 +0200

    fix: correction boucle infinie useEffect dans About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ function About() {
         fetch('https://jsonplaceholder.typicode.com/todos/1')
         .then(response => response.json())
         .then(json => setTodo(json))
-    });
+    }, []);
 
     return (
         <div>
